2fa3d9e95e0283f857cb3385dd6001cb4cc2adf4
Matthew Fritsch <matthewmfritsch@gmail.com>
2023-10-02 08:02:38 -0700

    added Lexeme notes added error reporting, sorta

--- com/matthewfritsch/Lox.java
+++ com/matthewfritsch/Lox.java
@@ -7,9 +7,9 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
 
 public class Lox {
+    static boolean hasError = false;
     public static void main(String[] args) {
         if(args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -26,6 +26,7 @@ public class Lox {
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path)); //get all bytes from the file specified at `path`
         run(new String(bytes, Charset.defaultCharset()));   //run() those bytes as a string
+        if (hadError) System.exit(65);
     }
 
     private static void runPrompt() throws IOException {
@@ -39,9 +40,17 @@ public class Lox {
             String line = reader.readLine();
             if (line == null) break;
             run(line);
+            hadError = false;
         }
     }
 
+    /*
+    NOTE: `var language = "lox";` is a valid line of Lox. The
+        lexemes in this line are ["var", "language", "=", "\"lox\""]
+        where a lexeme is the raw substring of code. A "token" is
+        more than just the raw substring, it includes the lexeme and
+        other stuff.
+    */
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
@@ -49,6 +58,23 @@ public class Lox {
         for(Token token : tokens) {
             System.out.println(token);
         }
+    }
+
+    /*
+    TODO: It's good to have error reporting. This is not great.
+        Should probably implement something a bit more descriptive,
+        as well as a stack to work backwards through. This is the
+        right place for error *reporting*, but not the right place
+        for error *generation*. TBD.
+    */
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println(
+            "[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
     }
 
 }
