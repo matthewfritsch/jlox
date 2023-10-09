fa483c4e967456d6ba96710a0f8c4bc867e6585e
Matthew Fritsch <matthewmfritsch@gmail.com>
2023-10-09 08:42:06 -0700

    Added hashmap for matching token types with their associated string changed the package path

--- com/matthewfritsch/Scanner.java
+++ com/matthewfritsch/Scanner.java
@@ -1,19 +1,40 @@
-package com.matthewfritsch.lox;
+package com.matthewfritsch;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.matthewfritsch.lox.TokenType;
+import com.matthewfritsch.TokenType;
 
 class Scanner {
-    private final String source;
-    private final List<Token> tokens = new ArrayList<>();
-
     private int start = 0;
     private int current = 0;
     private int line = 1;
+
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    TokenType.AND);
+        keywords.put("class",  TokenType.CLASS);
+        keywords.put("else",   TokenType.ELSE);
+        keywords.put("false",  TokenType.FALSE);
+        keywords.put("for",    TokenType.FOR);
+        keywords.put("fun",    TokenType.FUN);
+        keywords.put("if",     TokenType.IF);
+        keywords.put("nil",    TokenType.NIL);
+        keywords.put("or",     TokenType.OR);
+        keywords.put("print",  TokenType.PRINT);
+        keywords.put("return", TokenType.RETURN);
+        keywords.put("super",  TokenType.SUPER);
+        keywords.put("this",   TokenType.THIS);
+        keywords.put("true",   TokenType.TRUE);
+        keywords.put("var",    TokenType.VAR);
+        keywords.put("while",  TokenType.WHILE);
+    }
 
     Scanner(String source){
         this.source = source;
