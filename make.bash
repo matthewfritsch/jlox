5b1b8343151a1666a381d42aa2f74577db8b5075
Matthew Fritsch <matthewmfritsch@gmail.com>
2023-10-16 13:41:41 -0700

    fix for build/clean paths

--- make.bash
+++ make.bash
@@ -1,5 +1,5 @@
 export PROJ_DIR=$(pwd)
 
-alias clean="rm $PROJ_DIR/com/matthewfritsch/*.class"
-alias build="javac $PROJ_DIR/com/matthewfritsch/Lox.java"
+alias clean="rm $PROJ_DIR/com/matthewfritsch/*/*.class"
+alias build="javac $PROJ_DIR/com/matthewfritsch/lang/Lox.java"
 alias cbuild="clean && build"
