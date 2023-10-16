d2f8b99d689070a249b1cf2a8ce941b19186733b
Matthew Fritsch <matthewmfritsch@gmail.com>
2023-10-16 14:46:25 -0700

    build command is capable of building any directory now

--- make.bash
+++ make.bash
@@ -1,5 +1,24 @@
 export PROJ_DIR=$(pwd)
 
 alias clean="rm $PROJ_DIR/com/matthewfritsch/*/*.class"
-alias build="javac $PROJ_DIR/com/matthewfritsch/lang/Lox.java"
 alias cbuild="clean && build"
+
+function build {
+  A1=$1
+  if [ "$A1" == "" ]; then
+    A1="lang"
+  fi
+
+  found=0
+  for f in $(ls $PROJ_DIR/com/matthewfritsch); do
+    if [ $found == 0 ] && [ "$f" == "$A1" ]; then
+      found=1
+    fi
+  done
+
+  if [ $found == 1 ]; then
+    javac $PROJ_DIR/com/matthewfritsch/$A1/*.java
+  else
+    echo "$A1 is not in the directory."
+  fi
+}
