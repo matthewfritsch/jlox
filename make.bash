0b49bc67973660d4dfdc7fb64d36c61a9fe455cd
Matthew Fritsch <matthewmfritsch@gmail.com>
2023-10-17 09:13:41 -0700

    added more build functionality

--- make.bash
+++ make.bash
@@ -1,7 +1,20 @@
 export PROJ_DIR=$(pwd)
+export PROJ_PKG_EXT=com/matthewfritsch
+export PROJ_PKG_DIR=$PROJ_DIR/$PROJ_PKG_EXT
 
-alias clean="rm $PROJ_DIR/com/matthewfritsch/*/*.class"
+alias clean="rm $PROJ_PKG_DIR/*/*.class"
 alias cbuild="clean && build"
+alias fullbuild="clean; build util && build tool && java $PROJ_PKG_EXT/tool/GenerateAst $PROJ_PKG_EXT/lang && build lang"
+
+function exists {
+  found=1
+  for f in $(ls $PROJ_PKG_DIR); do
+    if [ $found == 1 ] && [ "$f" == "$A1" ]; then
+      found=0
+    fi
+  done
+  return $found
+}
 
 function build {
   A1=$1
@@ -9,16 +22,14 @@ function build {
     A1="lang"
   fi
 
-  found=0
-  for f in $(ls $PROJ_DIR/com/matthewfritsch); do
-    if [ $found == 0 ] && [ "$f" == "$A1" ]; then
-      found=1
-    fi
-  done
+  exists $A1
+  found=$?
 
-  if [ $found == 1 ]; then
-    javac $PROJ_DIR/com/matthewfritsch/$A1/*.java
+  if [ $found == 0 ]; then
+    javac $PROJ_PKG_DIR/$A1/*.java
   else
     echo "$A1 is not in the directory."
   fi
+
+  return $found
 }
