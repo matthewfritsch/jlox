e52714e54639db1d277962ece867ba3a22c97165
Matthew Fritsch <matthewmfritsch@gmail.com>
2023-10-20 09:09:22 -0700

    Added lots of functions for handling different kinds of expressions recursive descent works down all of the different functions

--- com/matthewfritsch/lang/Parser.java
+++ com/matthewfritsch/lang/Parser.java
@@ -15,6 +15,7 @@ class Parser {
         return equality();
     }
 
+    // equality -> comparison ( ( "!=" | "==" ) comparison )* ;
     private Expr equality() {
         Expr expr = comparison();
 
@@ -25,6 +26,69 @@ class Parser {
         }
 
         return expr;
+    }
+
+    // comparison -> term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+    private Expr comparison() {
+        Expr expr = term(); //if it's just a term, we never complete the while loop
+
+        while (match(TokenType.GREATER, TokenType.GREATER_EQUAL, TokenType.LESS, TokenType.LESS_EQUAL)) {
+            Token operator = previous();
+            Expr right = term();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr term() {
+        Expr expr = factor();
+
+        while (match(TokenType.MINUS, TokenType.PLUS)) {
+            Token operator = previous();
+            Expr right = factor();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr factor() {
+        Expr expr = unary();
+
+        while (match(TokenType.SLASH, TokenType.STAR)) {
+            Token operator = previous();
+            Expr right = unary();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr unary() {
+        if (match(TokenType.BANG, TokenType.MINUS)) {
+            Token operator = previous();
+            Expr right = unary();
+            expr = new Expr.Unary(operator, right);
+        }
+
+        return primary();
+    }
+
+    private Expr primary() {
+        if(match(TokenType.FALSE)) return new Expr.Literal(false);
+        if(match(TokenType.TRUE)) return new Expr.Literal(true);
+        if(match(TokenType.NIL)) return new Expr.Literal(null);
+
+        if(match(TokenType.NUMBER, TokenType.STRING)) {
+            return new Expr.Literal(previous().literal);
+        }
+        
+        if(match(TokenType.LEFT_PAREN)) {
+            Expr expr = expression();
+            consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.");
+            return new Expr.Grouping(expr);
+        }
     }
 
     private boolean match(TokenType... types) {
