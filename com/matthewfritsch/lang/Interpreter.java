0ea08c6a4837d27e0d4117c8f0b0ca3d8ae9488e
Matthew Fritsch <matthewmfritsch@gmail.com>
2023-11-01 09:09:25 -0700

    Added type checking for each operator we work with Added an interpret function so our interpreter can start... interpreting, I guess.

--- com/matthewfritsch/lang/Interpreter.java
+++ com/matthewfritsch/lang/Interpreter.java
@@ -2,6 +2,28 @@ package com.matthewfritsch.lang;
 
 class Interpreter implements Expr.Visitor<Object> {
 
+    void interpret(Expr expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+
+    private String stringify(Object object) {
+        if (object == null) return "nil";
+
+        if (object instanceof Double) {
+            String text = object.toString();
+            if(text.endsWith(".0")) {
+                text = text.substring(0, text.length()-2);
+            }
+            return text;
+        }
+        return object.toString();
+    }
+    
     //Literals should just be the value we interpret them as in Java
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
@@ -17,8 +39,14 @@ class Interpreter implements Expr.Visitor<Object> {
             case TokenType.BANG:
                 return !isTruthy(right);
             case TokenType.MINUS:
+                checkNumberOperand(expr.operator, right);
                 return -(double)right;
         }
+    }
+
+    private void checkNumberOperand(Token operator, Object operand) {
+        if (operand instanceof Double) return;
+        throw new RuntimeError(operator, "Operator must be a number.");
     }
 
     //Woo! Good truthy rules :)
@@ -37,8 +65,10 @@ class Interpreter implements Expr.Visitor<Object> {
 
         switch (expr.operator.type) {
             case TokenType.MINUS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left - (double)right;
             case TokenType.SLASH:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left / (double)right;
             case TokenType.PLUS:
                 if(left instanceof Double && right instanceof Double) {
@@ -47,21 +77,32 @@ class Interpreter implements Expr.Visitor<Object> {
                 if(left instanceof String && right instanceof String) {
                     return (String)left + (String)right
                 }
-                break;
+                
+                throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
             case TokenType.STAR:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left + (double)right;
             case TokenType.GREATER:
+                checkNumberOperands(expr.operator, left, right);
                 return double(left) > (double)right;
             case TokenType.GREATER_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return double(left) >= (double)right;
             case TokenType.LESS:
+                checkNumberOperands(expr.operator, left, right);
                 return double(left) < (double)right;
             case TokenType.LESS_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return double(left) <= (double)right;
             case TokenType.BANG_EQUAL: return !isEqual(left, right);
             case TokenType.EQUAL_EQUAL: return isEqual(left, right);
             
         }
+    }
+
+    private void checkNumberOperands(Token operator, Object left, Object right) {
+        if (left instanceof Double && right instanceof Double) return;
+        throw new RuntimeError(operator, "Operands must be numbers.");
     }
 
     private Object isEqual(Object a, Object b) {
