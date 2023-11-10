fc7aadc283c72a7cf4984e644922d1dd2ba7eb62
Matthew Fritsch <matthewmfritsch@gmail.com>
2023-11-10 09:13:29 -0800

    Added interpreter interpret, so now the interpreter is actually performing arithmetic. Nice!

--- com/matthewfritsch/lang/Lox.java
+++ com/matthewfritsch/lang/Lox.java
@@ -9,7 +9,10 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
+
     public static void main(String[] args) throws IOException {
         if(args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -27,6 +30,7 @@ public class Lox {
         byte[] bytes = Files.readAllBytes(Paths.get(path)); //get all bytes from the file specified at `path`
         run(new String(bytes, Charset.defaultCharset()));   //run() those bytes as a string
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -59,7 +63,7 @@ public class Lox {
 
         if (hadError) return;
 
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
     }
 
     /*
@@ -85,6 +89,11 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
 }
